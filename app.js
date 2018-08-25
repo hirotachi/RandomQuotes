2c1deabee3e81e884a9d0c8cd297a85dd04819a6
Hirotachi <milesfiles0@gmail.com>
2018-08-25 14:52:53 +0000

    use the request client to request random quotes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express"),
     app = express(),
-    bodyParser = require("body-parser");
+    bodyParser = require("body-parser"),
+    request = require("request");
 //APP SETUP
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -9,7 +10,14 @@ app.use(express.static(__dirname + '/public'));
 
 //ROUTES
 app.get("/", function (req, res) {
-    res.render("index");
+    res.redirect("/randomizer");
+});
+
+app.get("/randomizer", function (req, res) {
+    request("https://talaikis.com/api/quotes/random/", function(error, response, body){
+        var quote = JSON.parse(body);
+        res.render("index", {quote: quote});
+    });
 });
 
 
